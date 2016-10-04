299e4efbe4f8e6d8184ca2e89e3093522c9591e8
Developer University <bob@devu.com>
2016-10-04 07:40:45 -0500

    Initial check in

---
+++ Lesson-70-Mega-Solution-Part3/PapaBobs/PapaBobs.Persistence/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Persistence
+{
+    public class OrderRepository
+    {
+        public static void CreateOrder()
+        {
+            var db = new PapaBobsDbEntities();
+
+            var order = new Order();
+            order.OrderId = Guid.NewGuid();
+            order.Size = DTO.Enums.SizeType.Large;
+            order.Crust = DTO.Enums.CrustType.Thick;
+            order.Pepperoni = true;
+            order.Name = "Test";
+            order.Address = "123 Elm";
+            order.Zip = "12345";
+            order.Phone = "555-5555";
+            order.PaymentType = DTO.Enums.PaymentType.Credit;
+            order.TotalCost = 16.50M;
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+        }
+    }
+}
